8ede5c1ebd0264dee5f428474675d9a4a9cf1267
NurlanEmilbekuulu <nurlan.emilbekuulu.dev@gmail.com>
2020-09-16 17:14:57 +0600

    modified:   core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,16 @@
 from pathlib import Path
+import environ
 from django.utils.translation import gettext_lazy as _
+
+env = environ.Env()
+
+environ.Env.read_env('.env')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'adg&5rmow*ub4!9zlg$g5qz$mb5@g3mu^^gijcpa6n@1%i2h+s'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
@@ -51,8 +56,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
